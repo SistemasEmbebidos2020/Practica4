5a89b07a2aa70b43b13c6493205b0f02a5e65a1c
SistemasEmbebidos <78398897+SistemasEmbebidos2020@users.noreply.github.com>
2022-04-14 14:15:53 -0500

    Update BlinkLed.c

--- BlinkLed.c
+++ BlinkLed.c
@@ -1,18 +1,27 @@
-#include <avr/io.h> 
+#include <avr/io.h>
 #include <util/delay.h> 
-int main(void) 
-{ 
-DDRB |= ((1 << DDB5)); //asignar a puerto b5 como salida//pin 13 de arduino 
-DDRB |= ((1 << DDB3)); //asignar a puerto b3 como salida//pin 11 de arduino 
-DDRB |= ((1 << DDB2)); //asignar a puerto b2 como salida//pin 10 de arduino 
-while(1) 
-{ 
-PORTB = ((1 << PB2)); //asignar 1 al puerto b2 
-_delay_ms (1000); 
-PORTB = ((1 << PB3)); //asignar 1 al puerto b3 
-_delay_ms (1000); 
-PORTB = ((1 << PB5)); //asignar 1 al puerto b5 
-_delay_ms (1000); 
-} 
-return 0; 
-}
+
+int led0 = 0;
+int led1 = 1;
+int led2 = 2;
+
+int main()
+ { 
+    DDRB |= 1 << led0;
+    DDRB |= 1 << led1;
+    DDRB |= 1 << led2;
+
+    while (1){
+       
+       PORTB = 1<<led0;
+       _delay_ms(1000);
+       PORTB |= 1<<led1;
+       _delay_ms(1000);
+       PORTB |= 1<<led2;
+       _delay_ms(1000);
+       PORTB &= ~(1<<led2) & ~(1<<led1) & ~(1<<led0);
+       _delay_ms(1000);
+       }
+ return 0;
+       
+ }
