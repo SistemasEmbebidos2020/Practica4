dc347ba65aefe63fd46f11b01368571b4ecb4b62
SistemasEmbebidos <78398897+SistemasEmbebidos2020@users.noreply.github.com>
2022-06-06 06:12:31 -0500

    Update dcmotor.c

--- dcmotor.c
+++ dcmotor.c
@@ -2,19 +2,15 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-int ENA = 0;
-int IN1 = 1;
-int IN2 = 2;
-
 int main()
  { 
 	DDRB = 0b111;
    while (1)
 	{
-		PORTB = ((1<<ENA)|(1<<IN1)) & ~(1<<IN2);
+		PORTB = ((1<<PORTB0)|(1<<PORTB1)) & ~(1<<PORTB2);
 		_delay_ms(2000);
 
-		PORTB = ((1<<ENA)|(1<<IN2)) & ~(1<<IN1);
+		PORTB = ((1<<PORTB0)|(1<<PORTB2)) & ~(1<<PORTB1);
 		_delay_ms(1000);
 	}
    return 0;
