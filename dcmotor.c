c3eedb1f72ecd1cf25e862685ed311cafa939aa0
SistemasEmbebidos <78398897+SistemasEmbebidos2020@users.noreply.github.com>
2022-06-06 06:32:26 -0500

    Update dcmotor.c

--- dcmotor.c
+++ dcmotor.c
@@ -7,10 +7,10 @@ int main()
 	DDRB = 0b111;
    while (1)
 	{
-		PORTB = ((1<<PORTB0)|(1<<PORTB1)) & ~(1<<PORTB2);
+		PORTB = ((1<<PB0)|(1<<PB1)) & ~(1<<PB2);
 		_delay_ms(2000);
 
-		PORTB = ((1<<PORTB0)|(1<<PORTB2)) & ~(1<<PORTB1);
+		PORTB = ((1<<PB0)|(1<<PB2)) & ~(1<<PB1);
 		_delay_ms(1000);
 	}
    return 0;
