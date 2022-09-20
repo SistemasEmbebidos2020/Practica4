18dd18895e7306e9481cc05705e072635b78b9fc
SistemasEmbebidos <78398897+SistemasEmbebidos2020@users.noreply.github.com>
2022-09-20 13:44:27 -0500

    Update 7seg.c

--- 7seg.c
+++ 7seg.c
@@ -1,45 +1,32 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-
-const int demora = 1000; 
-char num[10] = {0B1000000,0B111001,0B0100100,0B0110000,0B0011001,0B0010010,0B0000011,0B1111000,0B0000000,0B0011000};
-int g = PD6;
+const int demora = 1000;
+char num[10] = {0B1000000,0B1111001,0B0100100,0B0110000,0B0011001,0B0010010,0B0000011,0B1111000,0B0000000,0B0011000};
 int main()
 { 
- DDRB = 0B11111111;
- DDRD = 1<<6;
-	while (1)
+ DDRD = 0B1111111;
+ 	while (1)
       {
-	    PORTB = num[0];
-		PORTD = (1<<g);
+	    	PORTD = num[0];
 		_delay_ms(demora);
-	        PORTB = num[1];
-		PORTD = (1<<g);
+		PORTD = num[1];
 		_delay_ms(demora);
-	        PORTB = num[2];
-		PORTD &= ~(1<<g);
+	        PORTD = num[2];
 		_delay_ms(demora);
-	        PORTB = num[3];
-		PORTD &= ~(1<<g);
+	        PORTD = num[3];
 		_delay_ms(demora);
-		PORTB = num[4];
-		PORTD &= ~(1<<g);
+		PORTD = num[4];
 		_delay_ms(demora);
-		PORTB = num[5];
-		PORTD &= ~(1<<g);
+		PORTD = num[5];
 		_delay_ms(demora);
-	        PORTB = num[6];
-		PORTD &= ~(1<<g);
+	        PORTD = num[6];
 		_delay_ms(demora);
-		PORTB = num[7];
-		PORTD = (1<<g);
+		PORTD = num[7];
 		_delay_ms(demora);
-		PORTB = num[8];
-		PORTD &= ~(1<<g);
+		PORTD = num[8];
 		_delay_ms(demora);
-		PORTB = num[9];
-		PORTD &= ~(1<<g);
+		PORTD = num[9];
 		_delay_ms(demora);
 
       }     
