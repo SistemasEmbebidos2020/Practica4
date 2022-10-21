d037e2e1487c056e81ef912eeb85ee182981fddf
SistemasEmbebidos <78398897+SistemasEmbebidos2020@users.noreply.github.com>
2022-10-21 09:16:22 -0500

    Update dcmotor.c

--- dcmotor.c
+++ dcmotor.c
@@ -3,14 +3,14 @@
 #include <util/delay.h>
 
 int main()
- { 
-	DDRB = 0b11;
+ { 	//se declara como salidas al PB1 y PB2, pin 9 y 10 del arduino
+	DDRB = 0b110;
    while (1)
 	{
-		PORTB = (1<<PB1) & ~(1<<PB2);
+		PORTB = (1<<PB1) & ~(1<<PB2);	//se habilita para que gire en una sentido
 		_delay_ms(2000);
 
-		PORTB = (1<<PB2) & ~(1<<PB1);
+		PORTB = (1<<PB2) & ~(1<<PB1);	//giro en otro sentido
 		_delay_ms(1000);
 	}
    return 0;
