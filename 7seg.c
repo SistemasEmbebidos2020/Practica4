767f4e85a30bb2b205995ed7cda625960b411209
SistemasEmbebidos <78398897+SistemasEmbebidos2020@users.noreply.github.com>
2022-10-23 21:32:46 -0500

    Update 7seg.c

--- 7seg.c
+++ 7seg.c
@@ -1,33 +1,32 @@
-#include <avr/io.h>
-#include <util/delay.h>
+#include <Arduino.h>
 
 const int demora = 1000;
 char num[10] = {0B1000000,0B1111001,0B0100100,0B0110000,0B0011001,0B0010010,0B0000011,0B1111000,0B0000000,0B0011000};
-int main()
-{ 
- DDRD = 0B1111111;
- 	while (1)
-      {
-	    	PORTD = num[0];
-		_delay_ms(demora);
-		PORTD = num[1];
-		_delay_ms(demora);
-	        PORTD = num[2];
-		_delay_ms(demora);
-	        PORTD = num[3];
-		_delay_ms(demora);
-		PORTD = num[4];
-		_delay_ms(demora);
-		PORTD = num[5];
-		_delay_ms(demora);
-	        PORTD = num[6];
-		_delay_ms(demora);
-		PORTD = num[7];
-		_delay_ms(demora);
-		PORTD = num[8];
-		_delay_ms(demora);
-		PORTD = num[9];
-		_delay_ms(demora);
 
-      }     
+void setup() {
+  DDRD = 0B1111111;
+}
+
+void loop() {
+  PORTD = num[0];
+	delay(demora);
+	PORTD = num[1];
+	delay(demora);
+	PORTD = num[2];
+	delay(demora);
+	PORTD = num[3];
+	delay(demora);
+	PORTD = num[4];
+	delay(demora);
+	PORTD = num[5];
+	delay(demora);
+	PORTD = num[6];
+	delay(demora);
+	PORTD = num[7];
+	delay(demora);
+	PORTD = num[8];
+	delay(demora);
+	PORTD = num[9];
+	delay(demora);
+
 }
