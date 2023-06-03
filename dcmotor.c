c3142b03116e8d03f863475d71173e4cde180934
SistemasEmbebidos <78398897+SistemasEmbebidos2020@users.noreply.github.com>
2023-06-03 08:45:18 -0500

    dcmotor.c

--- dcmotor.c
+++ dcmotor.c
@@ -12,3 +12,26 @@ int main()
 	}
    return 0;
  }
+
+//SEGUNDA OPCION CREANDO FUNCIONES
+/*void der(){
+	PORTB = 0b101; //se define
+}
+void izq(){
+	PORTB = 0b011;
+}
+
+int main()
+ { 	//se declara como salidas al PB1 y PB2, pin 9 y 10 del arduino
+	DDRB = 0b111;
+  PORTB = 0b101;
+   while (1)
+	{
+		der();
+		_delay_ms(2000);
+		izq();
+		_delay_ms(2000);
+	}
+   return 0;
+ }
+*/
